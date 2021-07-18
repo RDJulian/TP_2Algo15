c9d85acf7ce33e57b7cba581723d467ec837c1cd
RDJulian <86427486+RDJulian@users.noreply.github.com>
2021-07-18 13:47:36 -0300

    Main

---
+++ TP_2.py
@@ -0,0 +1,22 @@
+def main() -> None:
+    while not selector == "0":
+        selector = input("Ingrese una opcion:")
+        if selector == "1":
+            pass
+        if selector == "2":
+            pass
+        if selector == "3":
+            pass
+        if selector == "4":
+            pass
+        if selector == "5":
+            pass
+        if selector == "6":
+            pass
+        if selector == "7":
+            pass
+        if selector == "8":
+            pass
+
+
+main()
