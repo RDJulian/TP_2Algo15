b84c3c3509f1a20e272a365a0da96441dc1229a3
RDJulian <jruizd@fi.uba.ar>
2021-07-18 15:58:16 -0300

    Funcion ver archivos locales codeada

--- TP_2.py
+++ TP_2.py
@@ -1,8 +1,34 @@
-def archivos_local():
-    pass
+import os
 
 
-def archivos_remoto():
+def ver_archivos(path: str) -> None:  # Esta parte puede servir para otras funciones.
+    directorios = os.listdir(path)
+    print("")
+    for archivo in directorios:
+        print(archivo)
+
+
+def archivos_local() -> None:
+    selector = str()
+    path = os.getcwd()
+    while not selector == "salir":
+        ver_archivos(path)
+        selector = input(
+            """
+        Ingrese la carpeta a la que quiera ingresar,
+        atras para volver al directorio anterior,
+        salir para volver al menu principal: """
+        )
+        if selector == "atras":
+            path = os.path.dirname(path)
+        else:
+            if os.path.isdir(os.path.join(path, selector)):
+                path = os.path.join(path, selector)
+            else:
+                print("\nLa carpeta no existe, intente nuevamente.")
+
+
+def archivos_remoto() -> None:
     pass
 
 
@@ -21,8 +47,13 @@ def main() -> None:
         8. Salir"""
         )
         selector = input("\nIngrese una opcion: ")
-        if selector == "1":
-            pass
+        if selector == "1":  # a arbitrario por ahora.
+            a = str()
+            a = input("1 Local, 2 Remoto: ")
+            if a == "1":
+                archivos_local()
+            if a == "2":
+                archivos_remoto()
         if selector == "2":
             pass
         if selector == "3":
